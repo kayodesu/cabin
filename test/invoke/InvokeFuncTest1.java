555b8a01f70315fcf47386e0886726110c4264c4
LucasJia <jiayangcg@hotmail.com>
2018-11-02 03:41:50 +0800

    .

--- test/invoke/InvokeFuncTest1.java
+++ test/invoke/InvokeFuncTest1.java
@@ -2,10 +2,12 @@
 public class InvokeFuncTest1 {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++)
-            new InvokeFuncTest1().test(i);
+            System.out.println(new InvokeFuncTest1().test(i, i+1, i+2));
     }
     
-    private void test(int i) {
-        System.out.println(i);
+    private int test(int i, int j, int k) {
+        String s = i + "" + j + "" + k;
+        System.out.println(s);
+        return i + j + k;
     }
 }
