7b9040ad05cea861681f5f3f14a0bb4a1d986460
LucasJia <jiayangcg@hotmail.com>
2019-01-01 14:06:47 +0800

    add comment

--- src/rtda/heap/mm/halloc.c
+++ src/rtda/heap/mm/halloc.c
@@ -8,6 +8,7 @@
 
 void *halloc(size_t len)
 {
+    // 分配内存的时候就清空，后面创建对象时就不会有清空的动作了
     void *p = calloc(len, 1);  // todo 暂时先这么搞
 //    printf("-------------------------            %p, %d\n", p, len);
     if (p == NULL) {
