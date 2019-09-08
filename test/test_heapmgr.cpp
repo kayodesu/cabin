f97b9a5e77124eb69fe045f061d1b8e0f59c0f7c
kayodesu <kayodesu@outlook.com>
2019-09-08 14:08:11 +0800

    add test cases

--- test/test_heapmgr.cpp
+++ test/test_heapmgr.cpp
@@ -7,15 +7,47 @@
 
 using namespace std;
 
-int main()
+void initJVM(int argc, char* argv[]);
+
+void test_get_and_back()
+{
+    HeapMgr mgr;
+
+    cout << mgr.toString().c_str() << endl;
+
+    void *p = mgr.get(1024);
+    cout << endl << mgr.toString().c_str() << endl;
+
+    void *q = mgr.get(1024);
+    cout << endl << mgr.toString().c_str() << endl;
+
+    mgr.back(p, 1024);
+    cout << endl << mgr.toString().c_str() << endl;
+
+    mgr.back(q, 1024);
+    cout << endl << mgr.toString().c_str() << endl;
+}
+
+void test_stack_overflow_error()
 {
     HeapMgr mgr;
 
     while (true) {
-        void *p = mgr.get(1024);
-        if (p == nullptr)
-            break;
+        void *p = mgr.get(10240);
+        if (p == nullptr) {
+            cout << "never goes here." << endl;
+            return;
+        }
     }
+}
+
+int main()
+{
+    initJVM(0, nullptr);
+
+    test_get_and_back();
+    cout << "------------------------------------------------------" << endl;
+    test_stack_overflow_error();
 
     return 0;
 }
